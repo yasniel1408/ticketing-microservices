c438a80278e162217135914773d0a3a369daf92f
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-02-13 09:48:25 -0300

    update

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,19 +1,25 @@
+// ERRORS
 export { default as BadRequestError } from "./errors/bad-request-error";
 export { default as DatabaseConnectionError } from "./errors/database-connection-error";
 export { default as NotFoundError } from "./errors/not-found-error";
 export { default as RequestValidationError } from "./errors/request-validation-error";
 export { default as NotAuthorizedError } from "./errors/not-authorized-error";
 
+// JWT
 export { default as CreateJwt } from "./jwt/create-jwt";
 export { default as VerifyJwt } from "./jwt/verify-jwt";
 
+// MIDDLEWARES
 export { default as ErrorHandlerMiddleware } from "./middlewares/error-handler-middleware";
 export { default as VerifyErrorMiddleware } from "./middlewares/verify-error-middleware";
 export { default as VerifyCurrentUser } from "./middlewares/verify-current-user";
 export { default as RequiredUserAuthentication } from "./middlewares/required-user-authentication";
 
+// ROUTE
 export { default as RouteControllerBase } from "./route-controller-base";
 
+// UTILS
 export { default as EnvironmentsVerification } from "./utils/environments-verification";
 
+// INTERFACE
 export { default as CRUDRepository } from "./interfaces/crud-repository";
